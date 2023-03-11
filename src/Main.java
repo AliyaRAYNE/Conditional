250bee208f86d249717f354baab462c26da271ac
Moon <Aliya0609@mail.ru>
2023-03-11 13:41:16 +0500

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrise = 15000;
+        int mile = 20;
+        int bonus = ticketPrise / mile;
+
+        System.out.println(bonus);
+    }
+}
